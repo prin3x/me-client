ee3a1b757b572f07f253f77967d4ce1e1fed75d9
sarut <prince@Princes-MacBook-Pro.local>
2022-02-06 22:08:58 +0700

    add api url

--- config.ts
+++ config.ts
@@ -1,2 +1,3 @@
 export const FULL_CANLENDAR_LICENSE = process.env.NEXT_PUBLIC_FULL_CANLENDAR_LICENSE || ''
 export const ASSET_URL = process.env.NEXT_PUBLIC_S3_BUCKET || ''
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api'
