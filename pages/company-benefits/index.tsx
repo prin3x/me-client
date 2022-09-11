fc316e9a7d346112d236c1f348d732567383deb0
Sarut <prince@Princes-MacBook-Pro.local>
2022-09-11 18:15:28 +0700

    11/09/22

--- pages/company-benefits/index.tsx
+++ pages/company-benefits/index.tsx
@@ -1,7 +1,8 @@
-import React, { ReactElement } from 'react';
-import { Button, Col, Image, Row } from 'antd';
-import LayoutHOC from '../../layout/LayoutHOC';
-import CompanyBenefitHero from '../../components/company-benefit/CompanyBenefitHero';
+import React, { ReactElement } from "react";
+import { Button, Col, Image, Row } from "antd";
+import LayoutHOC from "../../layout/LayoutHOC";
+import CompanyBenefitHero from "../../components/company-benefit/CompanyBenefitHero";
+import CompanyBenefitsList from "../../components/company-benefit/CompanyBenefitsList";
 
 interface Props {}
 
@@ -10,24 +11,8 @@ function CompanyBenefits({}: Props): ReactElement {
     <LayoutHOC>
       <div>
         <CompanyBenefitHero />
-        <Row className='container mx-auto rounded-xl pt-5 mt-10 border-[3px] border-primary '>
-          <Col span={24}>
-            <Row>
-              <Image
-                src='/assets/Company_Benefit.png'
-                alt='Company_Benefit'
-                preview={false}
-              />
-            </Row>
-          </Col>
-        </Row>
-        <Row className='mt-5'>
-          <Col span={24}>
-            <Row justify='center'>
-              <Button size="middle" type='primary' style={{borderRadius: '36px', width: '14rem', lineHeight: 1, fontWeight: 'bold', height: '2.5rem'}}>ฉบับเต็มคลิกที่นี่</Button>
-            </Row>
-          </Col>
-        </Row>
+
+        <CompanyBenefitsList />
       </div>
     </LayoutHOC>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    listStyleType: {
+      none: "none",
+      disc: "disc",
+      decimal: "decimal",
+      square: "square",
+      roman: "upper-roman",
+      circle: "circle",
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
